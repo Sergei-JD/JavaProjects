75d910f30f76dc95d0c15a1c5a9fb34e1724a56c
Sergei-JD <sergshlyazhko@gmail.com>
2021-06-12 16:00:00 +0300

    simple interface and music

---
+++ ProjectStart/src/com/company/gui_and_music/MyDrawPanel.java
@@ -0,0 +1,20 @@
+package com.company.gui_and_music;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyDrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+//        g.setColor(Color.orange);
+//        g.fillRect(20, 50, 100, 100);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70, 70, 100, 100);
+    }
+}
